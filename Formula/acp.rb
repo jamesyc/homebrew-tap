248cd753021564f52711d26d63a68a947d4bde81
James Chang <jamesc@berkeley.edu>
2026-05-07 16:57:46 -0700

    Add acp formula

---
+++ Formula/acp.rb
@@ -0,0 +1,25 @@
+class Acp < Formula
+  include Language::Python::Virtualenv
+
+  desc "AirPyrt Tools for Apple AirPort and Time Capsule ACP"
+  homepage "https://github.com/jamesyc/airpyrt-tools"
+  url "https://github.com/jamesyc/airpyrt-tools/releases/download/v1.0.0/acp-1.0.0.tar.gz"
+  sha256 "2aecae1c546c968d6ec62b19cd6d1e43b3958faa7b8fb4a9336ef4a5bcc48f03"
+  license "MIT"
+
+  depends_on "python@3.14"
+
+  resource "pycryptodomex" do
+    url "https://files.pythonhosted.org/packages/source/p/pycryptodomex/pycryptodomex-3.23.0.tar.gz"
+    sha256 "71909758f010c82bc99b0abf4ea12012c98962fbf0583c2164f8b84533c2e4da"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "usage: acp", shell_output("#{bin}/acp --help")
+    assert_match "Supported properties", shell_output("#{bin}/acp --listprop")
+  end
+end
---
+++ README.md
@@ -0,0 +1,15 @@
+# Homebrew Tap
+
+Homebrew formulae for James Chang projects.
+
+## AirPyrt Tools
+
+```sh
+brew install jamesyc/tap/acp
+```
+
+After installation, run:
+
+```sh
+acp --help
+```
